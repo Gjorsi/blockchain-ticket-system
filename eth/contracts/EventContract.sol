f8603907b109068392404ee4362d6893bb8b6a83
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2020-01-20 16:43:15 +0100

    #8 - feature - owner options

--- eth/contracts/EventContract.sol
+++ eth/contracts/EventContract.sol
@@ -4,6 +4,7 @@ contract EventContract {
   address payable private owner;
   uint public available_tickets;
   uint public ticket_price;
+  bool public sale_active = true;
   mapping(address => Customer) private tickets;
   address[] private customers;
 
@@ -18,25 +19,11 @@ contract EventContract {
     owner = msg.sender;
   }
 
+// ----- Owner functions -----
+
   modifier onlyOwner {
     require(msg.sender == owner, "User was not authorized to call this function.");
     _;
-  }
-
-  function buy_tickets(uint requested_num_tickets) external payable {
-      require(requested_num_tickets <= available_tickets, "Not enough tickets available");
-      require(msg.value >= requested_num_tickets*ticket_price, "Not enough ether was sent.");
-      if(tickets[msg.sender].num_tickets == 0){
-          tickets[msg.sender].addr = msg.sender;
-          customers.push(msg.sender);
-      }
-      tickets[msg.sender].num_tickets += requested_num_tickets;
-      available_tickets -= requested_num_tickets;
-
-      // Return excessive funds
-      if(msg.value > requested_num_tickets*ticket_price) {
-          msg.sender.transfer(msg.value - requested_num_tickets*ticket_price);
-      }
   }
 
   function withdraw_funds() external payable onlyOwner {
@@ -54,5 +41,37 @@ contract EventContract {
         num_tickets[i] = tickets[customers[i]].num_tickets;
     }
     return (customers, num_tickets);
+  }
+
+  function stop_sale() external onlyOwner {
+    sale_active = false;
+  }
+
+  function continue_sale() external onlyOwner {
+    sale_active = true;
+  }
+
+  function add_tickets(uint additional_tickets) external onlyOwner {
+    available_tickets += additional_tickets;
+  }
+
+// ----- Public functions -----
+
+  function buy_tickets(uint requested_num_tickets) external payable {
+      require(requested_num_tickets <= available_tickets, "Not enough tickets available");
+      require(msg.value >= requested_num_tickets*ticket_price, "Not enough ether was sent.");
+      require(sale_active, "Ticket sale is closed by owner.");
+
+      if(tickets[msg.sender].num_tickets == 0){
+          tickets[msg.sender].addr = msg.sender;
+          customers.push(msg.sender);
+      }
+      tickets[msg.sender].num_tickets += requested_num_tickets;
+      available_tickets -= requested_num_tickets;
+
+      // Return excessive funds
+      if(msg.value > requested_num_tickets*ticket_price) {
+          msg.sender.transfer(msg.value - requested_num_tickets*ticket_price);
+      }
   }
 }
