ee26c8975684861408959242972741266699c34c
Joakim Algrøy <joakimalgroy@gmail.com>
2020-01-31 17:16:14 +0100

    Update buyback and gas tests for phase 2

--- eth/test/BuybackTest.js
+++ eth/test/BuybackTest.js
@@ -2,54 +2,52 @@ const EventContract = artifacts.require("EventContract");
 
 contract('EventContract', (accounts) => {
   let eventC;
-  const eth = 1e18; //1 eth in wei units
-  const ticket_price = 1e16; // 0.01 eth
-  let receipt = [];
-  let gasPrice = [];
-  let buyer = accounts[1];
+  let events = [];
   const owner = accounts[0];
 
   it('Setup', async () => {
     eventC = await EventContract.deployed();
-    let actual_n_tickets = await eventC.available_tickets();
-    let actual_ticket_price = await eventC.ticket_price();
-    console.log(`     Available tickets: ${actual_n_tickets}`);
-    console.log(`     Ticket price: ${parseFloat(actual_ticket_price)}`);
-    await eventC.buy_tickets(1, {from:accounts[9], value:ticket_price})
-  })
+  });
+
+  it('Create event', async () => {
+    await eventC.create_event(1000, 1e16.toString(), false, 0, {from:owner});
+    events.push({ id: 0, num_tickets: 1000, ticket_price: 1e16, per_customer_limit: false, max_per_customer: 0, owner: owner});
+  });
 
   it('Attempt to buy and return tickets', async () => {
+    let buyer = accounts[1];
     let tickets_to_buy = 2;
-    let tickets_available_before = parseFloat(await eventC.available_tickets());
+    let tickets_available_before = parseFloat((await eventC.get_event_info(events[0].id)).available_tickets);
     let balance_before = BigInt(await web3.eth.getBalance(buyer));
-    receipt.push(await eventC.buy_tickets(tickets_to_buy, {from:buyer, value:ticket_price*2}));
-    console.log(`     Gas used to buy ${tickets_to_buy} ticket(s): ${receipt[0].receipt.gasUsed}`);
+    let receipt = await eventC.buy_tickets(events[0].id, tickets_to_buy, {from:buyer, value:events[0].ticket_price*2});
+    console.log(`     Gas used to buy ${tickets_to_buy} ticket(s): ${receipt.receipt.gasUsed}`);
 
-    let tx = await web3.eth.getTransaction(receipt[0].tx);
-    let total_gas_cost = tx.gasPrice*receipt[0].receipt.gasUsed;
+    let tx = await web3.eth.getTransaction(receipt.tx);
+    let total_gas_cost = tx.gasPrice*receipt.receipt.gasUsed;
 
-    let buyers_tickets = parseFloat(await eventC.get_tickets(buyer, {from:owner}));
+    let buyers_tickets = parseFloat(await eventC.get_tickets(events[0].id, buyer, {from:events[0].owner}));
     assert.equal(buyers_tickets, tickets_to_buy);
 
-    let tickets_available_after = parseFloat(await eventC.available_tickets());
+    let tickets_available_after = parseFloat((await eventC.get_event_info(events[0].id)).available_tickets);
     assert.equal(tickets_available_before, tickets_available_after+buyers_tickets);
 
-    receipt.push(await eventC.return_tickets({from:buyer}));
+    receipt = await eventC.return_tickets(events[0].id, {from:buyer});
 
-    console.log(`     Gas used to return ${tickets_to_buy} ticket(s): ${receipt[1].receipt.gasUsed}`);
-    tickets_available_after = await eventC.available_tickets();
+    console.log(`     Gas used to return ${tickets_to_buy} ticket(s): ${receipt.receipt.gasUsed}`);
+    tickets_available_after = parseFloat((await eventC.get_event_info(events[0].id)).available_tickets);
     assert.equal(tickets_available_before, tickets_available_after);
 
-    tx = await web3.eth.getTransaction(receipt[1].tx);
-    total_gas_cost += tx.gasPrice*receipt[1].receipt.gasUsed;
+    tx = await web3.eth.getTransaction(receipt.tx);
+    total_gas_cost += tx.gasPrice*receipt.receipt.gasUsed;
     let balance_after = BigInt(await web3.eth.getBalance(buyer));
     assert.equal(balance_before, balance_after+BigInt(total_gas_cost));
 
   });
 
   it('Attempt to return non-existing tickets', async () => {
+    let buyer = accounts[2];
     try {
-      await eventC.return_tickets({from:buyer});
+      await eventC.return_tickets(events[0].id, {from:buyer});
       assert.fail("User without tickets was allowed to call buyback function.");
     } catch (error) {
       if(error.message.search('User does not own any tickets') > -1) {
--- eth/test/GasTest.js
+++ eth/test/GasTest.js
@@ -7,36 +7,39 @@ contract('EventContract', (accounts) => {
   const ticket_price = 1e16; // 0.01 eth
   let buyer = accounts[1];
   const owner = accounts[0];
+  let events = [];
 
   let gas = {}
 
   it('Measure gas usage', async () => {
-    eventC = await EventContract.new(100, String(ticket_price), false, 0);
-    let tx = await web3.eth.getTransactionReceipt(eventC.transactionHash);
-    gas['constructor'] = tx.gasUsed;
+    eventC = await EventContract.deployed();
+
+    let receipt = (await eventC.create_event(1000, ticket_price.toString(), false, 0, {from:owner})).receipt;
+    events.push({ id: 0, num_tickets: 1000, ticket_price: 1e16, per_customer_limit: false, max_per_customer: 0, owner: owner});
+    gas['create_event'] = receipt.gasUsed;
 
     let tickets_to_buy = 1;
 
     // Buy ticket
-    let purchase = await eventC.buy_tickets(tickets_to_buy, {from:buyer, value:ticket_price});
+    let purchase = await eventC.buy_tickets(events[0].id, tickets_to_buy, {from:buyer, value:ticket_price});
     gas['buy_ticket'] = purchase.receipt.gasUsed;
 
     // Buy ticket with too much money
     buyer = accounts[3];
     let amount = 2*ticket_price;
-    purchase = await eventC.buy_tickets(tickets_to_buy, {from:buyer, value:amount});
+    purchase = await eventC.buy_tickets(events[0].id, tickets_to_buy, {from:buyer, value:amount});
     gas['buy_ticket_excessive'] = purchase.receipt.gasUsed;
 
     // Stop sale
-    let action = await eventC.stop_sale({from:owner});
+    let action = await eventC.stop_sale(events[0].id, {from:owner});
     gas['stop_sale'] = action.receipt.gasUsed;
 
     // Continue sale
-    action = await eventC.continue_sale({from:owner});
+    action = await eventC.continue_sale(events[0].id, {from:owner});
     gas['continue_sale'] = action.receipt.gasUsed;
 
     // Add tickets
-    action = await eventC.add_tickets(10, {from:owner});
+    action = await eventC.add_tickets(events[0].id, 10, {from:owner});
     gas['add_tickets'] = action.receipt.gasUsed;
 
     Object.keys(gas).map((action) => {
