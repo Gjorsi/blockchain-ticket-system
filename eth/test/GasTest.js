fcb9a37e37b07c847e165b9df56e3a90720c97a6
Joakim Algrøy <joakimalgroy@gmail.com>
2020-02-13 12:37:39 +0100

    Refactor gas test to use averages.

--- eth/test/GasTest.js
+++ eth/test/GasTest.js
@@ -7,7 +7,7 @@ contract('EventContract - Gas measurements', (accounts) => {
   const ticket_price = 1e16; // 0.01 eth
   let buyer = accounts[1];
   const owner = accounts[0];
-  let events = [];
+  let test_event;
 
   let gas = {}
 
@@ -17,33 +17,52 @@ contract('EventContract - Gas measurements', (accounts) => {
     let id = web3.utils.asciiToHex("TestEvent3");
     let title = web3.utils.asciiToHex("This is the event title");
 
+    // Test event for later tests
     let receipt = (await eventC.create_event(id, title, [1000], [ticket_price.toString()], false, 0, true, true, {from:owner})).receipt;
-    events.push({ id: id, num_tickets: 1000, ticket_price: 1e16, per_customer_limit: false, max_per_customer: 0, owner: owner});
-    gas['create_event'] = receipt.gasUsed;
+    test_event = { id: id, num_tickets: 1000, ticket_price: 1e16, per_customer_limit: false, max_per_customer: 0, owner: owner};
+
+    // Create event
+    gas['create_event'] = await Promise.all(accounts.map(async (acc) => {
+      // Create one event per account
+      return eventC.create_event(acc, title, [1000], [ticket_price.toString()], false, 0, true, true, {from:acc}); 
+    })).then((receipts) => {
+      let total = receipts.map((r) => r.receipt.gasUsed).reduce((a,b) => a+b,0); // Sum gas
+      return Math.round(total / accounts.length); // Calculate average
+    });
 
     let tickets_to_buy = 1;
 
     // Buy ticket
-    let purchase = await eventC.buy_tickets(events[0].id, 0, tickets_to_buy, {from:buyer, value:ticket_price});
-    gas['buy_ticket'] = purchase.receipt.gasUsed;
-
-    // Buy ticket with too much money
-    buyer = accounts[3];
-    let amount = 2*ticket_price;
-    purchase = await eventC.buy_tickets(events[0].id, 0, tickets_to_buy, {from:buyer, value:amount});
-    gas['buy_ticket_excessive'] = purchase.receipt.gasUsed;
+    gas['buy_ticket'] = await Promise.all(accounts.map(async (acc) => {
+      return eventC.buy_tickets(test_event.id, 0, 1, {from: acc, value: ticket_price}) // Buy tickets from all accounts
+    })).then((receipts) => {
+      let total = receipts.map((r) => r.receipt.gasUsed).reduce((a,b) => a+b); // Sum gas
+      return Math.round(total / accounts.length); // Calculate average
+    });
 
     // Stop sale
-    let action = await eventC.stop_sale(events[0].id, {from:owner});
+    let action = await eventC.stop_sale(test_event.id, {from:owner});
     gas['stop_sale'] = action.receipt.gasUsed;
 
     // Continue sale
-    action = await eventC.continue_sale(events[0].id, {from:owner});
+    action = await eventC.continue_sale(test_event.id, {from:owner});
     gas['continue_sale'] = action.receipt.gasUsed;
 
     // Add tickets
-    action = await eventC.add_tickets(events[0].id, [10], {from:owner});
+    action = await eventC.add_tickets(test_event.id, [10], {from:owner});
     gas['add_tickets'] = action.receipt.gasUsed;
+
+    // Return tickets
+    gas['return_tickets'] = await Promise.all(accounts.map(async (acc) => {
+      return eventC.return_tickets(test_event.id, {from:acc}); // Return tickets from all accounts
+    })).then((receipts) => {
+      let total = receipts.map((r) => r.receipt.gasUsed).reduce((a,b) => a+b,0); // Sum gas
+      return Math.round(total / accounts.length); // Calculate average
+    });
+    
+    // Delete event
+    action = await eventC.delete_event(test_event.id, {from: owner});
+    gas['delete_event'] = action.receipt.gasUsed;
 
     Object.keys(gas).map((action) => {
       gas[action] = gas[action].toLocaleString('nb-NO');
