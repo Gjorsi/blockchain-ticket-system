153045dd3799e8d61659195f765cd9678c33fdfe
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2020-02-17 16:42:03 +0100

    #30 - feature - Unfinished work on event creation (ticket pricing)

--- web/src/components/createEvent.js
+++ web/src/components/createEvent.js
@@ -3,8 +3,8 @@ import { Switch, TextField, FormControlLabel } from '@material-ui/core';
 
 export default class CreateEvent extends Component {
 	state = {event_ID: null, event_title: null, sale_active: false, buyback_active: false, customer_limited: false, 
-				tickets_per_customer: 0}
-
+				tickets_per_customer: 0, ticket_types: 1, price_table: [], ticket_avail: [], ticket_prices: []};
+	
 	render() {
 		return (
 			<div>
@@ -37,6 +37,22 @@ export default class CreateEvent extends Component {
 					label="Limit number of tickets per customer" /></div>
 				
 				{this.tickets_per_customer()}
+
+				<div><TextField 
+					id="ticket_types" 
+					label="# of ticket types" 
+					variant="outlined" 
+					margin="normal"
+					required={true}
+					type="number"
+					defaultValue={1}
+					inputProps={{ min: "1", max: "50", step: "1" }}
+					onChange={e => this.set_ticket_types(e.target.value)} /></div>
+
+				{this.state.price_table}
+
+				{this.ticket_avail}
+				{this.ticket_prices}
 					
 			</div>
 		);
@@ -44,13 +60,51 @@ export default class CreateEvent extends Component {
 
 	tickets_per_customer() {
 		if (this.state.customer_limited) return (
-			<TextField 
+			<div><TextField 
 				id="tickets_per_customer" 
 				label="Max. tickets / customer" 
 				variant="outlined" 
+				margin="normal"
 				required={true}
 				type="number"
-				onChange={e => this.setState({tickets_per_customer: e.target.value})} />
+				defaultValue={1}
+				inputProps={{ min: "1", step: "1" }}
+				onChange={e => this.setState({tickets_per_customer: e.target.value})} /></div>
 		)
+	}
+
+	set_ticket_types(val) {
+		this.setState({ticket_types: val});
+		this.setState({ticket_avail: new Array(val).fill(1)});
+		this.setState({ticket_prices: new Array(val).fill(1)});
+		this.tickets_and_prices();
+	}
+
+	tickets_and_prices() {
+		this.setState({price_table: []});
+		for (let i=0; i<this.state.ticket_types; i++) {
+			this.setState(prevState => ({ price_table: [prevState.price_table, (
+				<div key={i}><TextField 
+					id={"tickets_avail" + i}
+					label="Total # of tickets"
+					variant="outlined" 
+					margin="normal"
+					required={true}
+					type="number"
+					defaultValue={this.state.ticket_avail[i]}
+					inputProps={{ min: "1", step: "1" }}
+					onChange={e => this.state.ticket_avail[i] = e.target.value} />
+				<TextField 
+					id={"ticket_price" + i}
+					label="Ticket price" 
+					variant="outlined" 
+					margin="normal"
+					required={true}
+					type="number"
+					defaultValue={this.state.ticket_prices[i]}
+					inputProps={{ min: "1", step: "1" }}
+					onChange={e => this.state.ticket_prices[i] = e.target.value} /></div>
+			)]}))
+		}
 	}
 }
